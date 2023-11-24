71563fb4b93007615789af540020cea8fc9a1b76
jaxnalia <jacksendall@gmail.com>
2023-11-24 19:13:57 +0000

    links fixed

--- src/lib/components/NavBar/NavBarSheet.svelte
+++ src/lib/components/NavBar/NavBarSheet.svelte
@@ -1,6 +1,7 @@
 <script>
 	import * as Sheet from '$lib/components/ui/sheet';
 	import NavLogo from './NavLogo.svelte'
+	import { Separator } from "$lib/components/ui/separator";
 	let side = 'left';
 </script>
 
@@ -10,14 +11,15 @@
 	</Sheet.Trigger>
 	<Sheet.Content {side}>
 		<div class="p-4">
-			<h1 class="text-2xl font-semibold">3rd Planet Studio</h1>
+			<h1 class="text-2xl font-semibold">LEAN</h1>
 			<ul class="mt-4">
-				<li class="mb-2"><a href="/home" class="block hover:text-indigo-400">Home</a></li>
-				<li class="mb-2"><a href="/about" class="block hover:text-indigo-400">About</a></li>
-				<li class="mb-2"><a href="/events" class="block hover:text-indigo-400">Events</a></li>
-				<li class="mb-2"><a href="/services" class="block hover:text-indigo-400">Services</a></li>
-				<li class="mb-2"><a href="/roster/art" class="block hover:text-indigo-400">Art</a></li>
-				<li class="mb-2"><a href="/roster/music" class="block hover:text-indigo-400">Music</a></li>
+				<li class="mb-2"><a href="/" class="block hover:text-indigo-400">Home</a></li>
+				<li class="mb-2"><a href="/farm" class="block hover:text-indigo-400">Farm</a></li>
+				<li class="mb-2"><a href="https://www.geckoterminal.com/pulsechain/pools/0x9961c2652b301c4a25256db05316d2be11cebab1" target="_blank" class="block hover:text-indigo-400">Chart</a></li>
+				<Separator class="my-4" />
+				<li class="mb-2"><a href="https://t.me/plsLEAN" target="_blank" class="block hover:text-indigo-400">Telegram</a></li>
+				<li class="mb-2"><a href="http://x.com/pls_lean" target="_blank" class="block hover:text-indigo-400">Twitter</a></li>
+				<li class="mb-2"><a href="https://discord.gg/NRg34Rawc6" target="_blank" class="block hover:text-indigo-400">Discord</a></li>
 			</ul>
 		</div>
 	</Sheet.Content>
